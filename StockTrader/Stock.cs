ec67c22da7dda2ea3849e303956f4fee234b59b0
Peter Parthimos <peterparthimos@hotmail.com>
2020-12-04 19:58:38 -0500

    Created Stock and User Classes

---
+++ StockTrader/Stock.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockTrader
+{
+    class Stock
+    {
+        private string symbol;
+        private decimal open;
+        private decimal high;
+        private decimal low;
+        private decimal close;
+        private int quantity;
+
+        public Stock(string symbol, decimal open, decimal high, decimal low, decimal close, int quantity)
+        {
+            this.symbol = symbol;
+            this.open = open;
+            this.high = high;
+            this.low = low;
+            this.close = close;
+            this.quantity = quantity;
+        }
+    }
+}
---
+++ StockTrader/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockTrader
+{
+    class User
+    {
+        private decimal cashBalance;
+        private decimal bookCost;
+        private decimal marketValue;
+        private decimal totalValue;
+        private string lastLogin;
+        private List<Stock> stocks;
+
+        public User()
+        {
+            //TODO
+        }
+    }
+}
